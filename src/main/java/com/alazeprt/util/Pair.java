ec447cd745def716871c49f6804c51d5d57dd937
alazeprt <nono135246@126.com>
2023-06-24 10:13:16 +0800

    Update Pair.java

--- src/main/java/com/alazeprt/util/Pair.java
+++ src/main/java/com/alazeprt/util/Pair.java
@@ -32,7 +32,7 @@ public class Pair<K,V> implements Serializable{
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o instanceof javafx.util.Pair) {
+        if (o instanceof Pair) {
             Pair pair = (Pair) o;
             if (key != null ? !key.equals(pair.key) : pair.key != null) return false;
             if (value != null ? !value.equals(pair.value) : pair.value != null) return false;
