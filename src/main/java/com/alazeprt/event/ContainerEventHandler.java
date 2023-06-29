e4758b4a1e03904342209ec48770783a7043eef9
alazeprt <nono135246@126.com>
2023-06-29 21:43:40 +0800

    Update Checks

--- src/main/java/com/alazeprt/event/ContainerEventHandler.java
+++ src/main/java/com/alazeprt/event/ContainerEventHandler.java
@@ -13,13 +13,15 @@ public class ContainerEventHandler implements Listener {
 
     @EventHandler
     public void onPlayerInterfact(PlayerInteractEvent event){
-        Residence res = Residence.getResidenceByLocation(event.getClickedBlock().getLocation());
-        if(res != null){
-            if(!res.getSavedPlayer().equals(event.getPlayer().getName())){
-                Material clickedBlockMaterial = event.getClickedBlock().getBlockData().getMaterial();
-                if (clickedBlockMaterial.equals(Material.CHEST) || clickedBlockMaterial.equals(Material.BARREL) || clickedBlockMaterial.equals(Material.HOPPER) || clickedBlockMaterial.equals(Material.CHEST_MINECART) || clickedBlockMaterial.equals(Material.HOPPER_MINECART) || clickedBlockMaterial.equals(Material.TRAPPED_CHEST) || clickedBlockMaterial.name().endsWith("_SHULKER_BOX")) {
-                    event.setCancelled(true);
-                    event.getPlayer().sendMessage(getPrefixW() + ChatColor.RED + "你没有权限在他人领地上与容器方块交互!");
+        if(event.getClickedBlock() != null) {
+            Residence res = Residence.getResidenceByLocation(event.getClickedBlock().getLocation());
+            if(res != null){
+                if(!res.getSavedPlayer().equals(event.getPlayer().getName())){
+                    Material clickedBlockMaterial = event.getClickedBlock().getBlockData().getMaterial();
+                    if (clickedBlockMaterial.equals(Material.CHEST) || clickedBlockMaterial.equals(Material.BARREL) || clickedBlockMaterial.equals(Material.HOPPER) || clickedBlockMaterial.equals(Material.CHEST_MINECART) || clickedBlockMaterial.equals(Material.HOPPER_MINECART) || clickedBlockMaterial.equals(Material.TRAPPED_CHEST) || clickedBlockMaterial.name().endsWith("_SHULKER_BOX")) {
+                        event.setCancelled(true);
+                        event.getPlayer().sendMessage(getPrefixW() + ChatColor.RED + "你没有权限在他人领地上与容器方块交互!");
+                    }
                 }
             }
         }
