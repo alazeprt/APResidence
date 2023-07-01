3424832187dc239c3e88fcf99005fba5cc0ee3bc
alazeprt <nono135246@126.com>
2023-07-01 17:15:18 +0800

    Register TabCompleter

--- src/main/java/com/alazeprt/command/RegisterCommmand.java
+++ src/main/java/com/alazeprt/command/RegisterCommmand.java
@@ -7,5 +7,6 @@ import java.util.Objects;
 public class RegisterCommmand {
     public static void register(JavaPlugin plugin){
         Objects.requireNonNull(plugin.getCommand("apres")).setExecutor(new APCommand());
+        Objects.requireNonNull(plugin.getCommand("apres")).setTabCompleter(new APCommand());
     }
 }
